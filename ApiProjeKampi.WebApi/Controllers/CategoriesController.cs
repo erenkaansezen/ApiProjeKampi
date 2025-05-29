8bbbf8b2cd07de3a61d84a4f837a1fc34c77cc13
eren.sezen <eren.sezen@Sezen>
2025-05-29 21:57:14 +0000

    Add project files.

---
+++ ApiProjeKampi.WebApi/Controllers/CategoriesController.cs
@@ -0,0 +1,55 @@
+﻿using ApiProjeKampi.WebApi.Context;
+using ApiProjeKampi.WebApi.Entities;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ApiProjeKampi.WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CategoriesController : ControllerBase
+    {
+        private readonly ApiContext _context;
+        public CategoriesController(ApiContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public IActionResult CategoryList()
+        {
+            var categories = _context.Categories.ToList();
+            return Ok(categories);
+        }
+
+        [HttpPost]
+        public IActionResult CreateCategory(Category category)
+        {
+            _context.Categories.Add(category);
+            _context.SaveChanges();
+            return Ok("Kategori ekleme işlemi başarılı");
+        }
+
+        [HttpDelete]
+        public IActionResult DeleteCategory(int id)
+        {
+            var category = _context.Categories.Find(id);
+            _context.Categories.Remove(category);
+            return Ok("Kategori silme işlemi başarılı");
+        }
+
+        [HttpGet("GetCategory")]
+        public IActionResult GetCategory(int id)
+        {
+            var category = _context.Categories.Find(id);
+            return Ok(category);
+        }
+
+        [HttpPut]
+        public IActionResult UpdateCategory(Category category)
+        {
+            _context.Categories.Update(category);
+            _context.SaveChanges();
+            return Ok("Kategori Güncelleme İşlemi Başarılı");
+        }
+    }
+}
